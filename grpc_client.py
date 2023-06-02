f25b34d810f10da4ad0870c1171718b41bd4ce3d
Enrique <enri_lee@usp.br>
2023-06-02 17:08:15 -0300

    test added

--- grpc_client.py
+++ grpc_client.py
@@ -25,6 +25,7 @@ def emptyTest(stub):
     end = time.time()
     time_taken['empty'].append(end - start)
 
+
 def stringTest(stub):
     for i in range(10):
         string = 'aaaaa' * pow(4, i)
@@ -36,18 +37,33 @@ def stringTest(stub):
         time_taken['string'].append(end - start)
 
 
+def int32Test(stub):
+    data = grpc_service_pb2.int32Data(int32_data=123)
+    start = time.time()
+    for k in range(1000):
+        stub.int32Return(data)
+    end = time.time()
+    time_taken['int32'].append(end - start)
+
+
+def doubleTest(stub):
+    data = grpc_service_pb2.doubleData(double_data=111111.1111111)
+    start = time.time()
+    for k in range(1000):
+        stub.doubleReturn(data)
+    end = time.time()
+    time_taken['double'].append(end - start)
+
+
 def main():
     channel = grpc.insecure_channel('[::]:50051')
     stub = grpc_service_pb2_grpc.serviceTestStub(channel)
 
     emptyTest(stub)
     stringTest(stub)
+    int32Test(stub)
+    doubleTest(stub)
 
-    int_data = grpc_service_pb2.int32Data(int32_data=123)
-    double_data = grpc_service_pb2.doubleData(double_data=12312.123123)
-
-    stub.int32Return(int_data)
-    stub.doubleReturn(double_data)
     print(time_taken)
 
 
